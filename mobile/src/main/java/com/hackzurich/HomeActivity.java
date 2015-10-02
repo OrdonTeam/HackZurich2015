115118994460d97e05121494ddea5db88012ff01
ordon <ordon1313@gmail.com>
2015-10-03 00:03:56 +0200

    download button

--- mobile/src/main/java/com/hackzurich/HomeActivity.java
+++ mobile/src/main/java/com/hackzurich/HomeActivity.java
@@ -2,6 +2,9 @@ package com.hackzurich;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.OnClick;
 
 public class HomeActivity extends Activity {
 
@@ -9,5 +12,10 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+    }
+
+    @OnClick(R.id.download)
+    public void onDownload() {
+        Toast.makeText(this,"On download",Toast.LENGTH_LONG).show();
     }
 }

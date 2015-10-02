ba7e385d0fef23cc114c9043ceddc836267ddd39
Szop-kradziej <karolina.drabarz@gmail.com>
2015-10-03 00:45:07 +0200

    Fix compile errors

--- mobile/src/main/java/com/hackzurich/QuestionActivity.java
+++ mobile/src/main/java/com/hackzurich/QuestionActivity.java
@@ -11,28 +11,21 @@ public class QuestionActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+        setContentView(R.layout.question_activity);
+        addHeader();
+        insertAnswers();
+        addFooter();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_question, menu);
-        return true;
+    private void addHeader() {
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void insertAnswers() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void addFooter() {
+
     }
 }

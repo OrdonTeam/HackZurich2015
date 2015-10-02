575041e01b43fae976a0e3ced13bac6e4061fa23
ordon <ordon1313@gmail.com>
2015-10-03 00:37:46 +0200

    start learn

--- mobile/src/main/java/com/hackzurich/HomeActivity.java
+++ mobile/src/main/java/com/hackzurich/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.hackzurich;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class HomeActivity extends Activity {
@@ -12,10 +14,16 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        ButterKnife.bind(this);
     }
 
     @OnClick(R.id.download)
     public void onDownload() {
-        Toast.makeText(this,"On download",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "On download", Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.learn)
+    public void onLearn() {
+        startActivity(new Intent(this, StartLearn.class));
     }
 }

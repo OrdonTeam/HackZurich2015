6687deb7b9a7adca32a5581e2db43fa00d932324
ordon <ordon1313@gmail.com>
2015-10-03 00:49:27 +0200

    start learn

---
+++ mobile/src/main/java/com/hackzurich/StartLearn.java
@@ -0,0 +1,13 @@
+package com.hackzurich;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public final class StartLearn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_learn_activity);
+    }
+}

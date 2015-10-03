05d33bccb524049a458e46cb6c10e111308dc064
ordon <ordon1313@gmail.com>
2015-10-03 04:14:31 +0200

    butter knife

--- mobile/src/main/java/com/hackzurich/QuestionActivity.java
+++ mobile/src/main/java/com/hackzurich/QuestionActivity.java
@@ -4,72 +4,49 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.hackzurich.adapter.AnswerGroupAdapter;
 import com.hackzurich.model.Answer;
 import com.hackzurich.model.Question;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class QuestionActivity extends Activity {
 
     public static final String QUESTION = "question";
-    AnswerGroupAdapter answerGroupAdapter;
+
+    private AnswerGroupAdapter answerGroupAdapter = new AnswerGroupAdapter();
+
+    @Bind(R.id.answerListView)
+    ListView answerListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.question_activity);
-        addAnswerListAdapter();
-        addFooterButtonsListeners();
-        addExampleAnswers();
+        ButterKnife.bind(this);
+        setAdapter();
     }
 
-    private void addAnswerListAdapter() {
-        answerGroupAdapter = new AnswerGroupAdapter();
-        ListView answerListView = (ListView) findViewById(R.id.answerListView);
-        answerListView.setAdapter(answerGroupAdapter);
-    }
-
-    private void addFooterButtonsListeners() {
-        addDoNotKnowButtonListener();
-        addConfirmAnswerButtonListener();
-    }
-
-    private void addDoNotKnowButtonListener() {
-        Button doNotKnowButton = (Button) findViewById(R.id.doNotKnowButton);
-        doNotKnowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doNotKnowButtonClicked();
-            }
-        });
-    }
-
-    private void doNotKnowButtonClicked() {
-
-    }
-
-    private void addConfirmAnswerButtonListener() {
-        Button confirmAnswerButton = (Button) findViewById(R.id.confirmAnswerButton);
-        confirmAnswerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                confirmAnswerButtonClicked();
-            }
-        });
-    }
-
-    private void confirmAnswerButtonClicked() {
-        answerGroupAdapter.setSubmitted();
-    }
-
-    private void addExampleAnswers() {
+    private void setAdapter() {
         Question question = (Question) getIntent().getSerializableExtra(QUESTION);
         for (Answer answer : question.getAnswers()) {
             answerGroupAdapter.addAnswerItem(new AnswerItem(answer));
         }
+        answerListView.setAdapter(answerGroupAdapter);
+    }
+
+    @OnClick(R.id.confirmAnswerButton)
+    public void confirmAnswerButtonClicked() {
+        answerGroupAdapter.setSubmitted();
+    }
+
+    @OnClick(R.id.doNotKnowButton)
+    public void doNotKnowButtonClicked() {
+
     }
 
     public static Intent getIntent(Context context, Question question) {

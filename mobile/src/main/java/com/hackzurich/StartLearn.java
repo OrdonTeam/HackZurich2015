c8757fde6aaed01b96ffd83114d0e63a087e4a1c
ordon <ordon1313@gmail.com>
2015-10-03 03:15:19 +0200

    colors :)

--- mobile/src/main/java/com/hackzurich/StartLearn.java
+++ mobile/src/main/java/com/hackzurich/StartLearn.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.hackzurich.model.TestWrapper;
@@ -11,6 +12,7 @@ import com.hackzurich.model.data.TestData;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.hackzurich.model.data.QuestionDataStatus.EASY;
 import static com.hackzurich.model.data.QuestionDataStatus.HARD;
@@ -41,9 +43,14 @@ public final class StartLearn extends Activity {
     protected void onResume() {
         super.onResume();
         TestData testData = testWrapper.getTestData();
-        hardView.setText("Easy (" + testData.countOf(EASY) + ")");
-        hardView.setText("Medium (" + testData.countOf(MEDIUM) + ")");
+        easyView.setText("Easy (" + testData.countOf(EASY) + ")");
+        mediumView.setText("Medium (" + testData.countOf(MEDIUM) + ")");
         hardView.setText("Hard (" + testData.countOf(HARD) + ")");
+    }
+
+    @OnClick({R.id.easy, R.id.medium, R.id.hard})
+    public void onEasy(View view) {
+        view.setSelected(!view.isSelected());
     }
 
     public static void start(Context context, TestWrapper test) {

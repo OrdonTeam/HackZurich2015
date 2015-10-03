791bd49bb25874a94bfdeb5fe81c80f8bad0b4ff
ordon <ordon1313@gmail.com>
2015-10-03 19:32:20 +0200

    small step 1

--- wear/src/main/java/com/hackzurich/second/SecondFragment.java
+++ wear/src/main/java/com/hackzurich/second/SecondFragment.java
@@ -21,7 +21,7 @@ public final class SecondFragment extends CardFragment {
     private final int[] answerIds = {R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4, R.id.answer5, R.id.answer6, R.id.answer7};
     private final int[] answerTextIds = {R.id.answerText1, R.id.answerText2, R.id.answerText3, R.id.answerText4, R.id.answerText5, R.id.answerText6, R.id.answerText7};
     private final List<View> answerViews = new ArrayList<>();
-    private final List<View> answerTextViews = new ArrayList<>();
+    private final List<TextView> answerTextViews = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,13 +47,13 @@ public final class SecondFragment extends CardFragment {
             answerViews.add(getView().findViewById(answerIds[i]));
         }
         for (int i = 0; i < question.getAnswers().size(); i++) {
-            answerTextViews.add(getView().findViewById(answerTextIds[i]));
+            answerTextViews.add((TextView) getView().findViewById(answerTextIds[i]));
         }
     }
 
     private void initQuestions() {
         for (int i = 0; i < question.getAnswers().size(); i++) {
-            View answerView = getView().findViewById(answerIds[i]);
+            View answerView = answerViews.get(i);
             answerView.setVisibility(View.VISIBLE);
             answerView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -62,7 +62,7 @@ public final class SecondFragment extends CardFragment {
                     setFlipper();
                 }
             });
-            TextView textView = (TextView) getView().findViewById(answerTextIds[i]);
+            TextView textView = answerTextViews.get(i);
             textView.setText(question.getAnswers().get(i).getText());
         }
     }
@@ -86,7 +86,7 @@ public final class SecondFragment extends CardFragment {
 
     private boolean isAnyAnswerChoosen() {
         for (View view : answerViews) {
-            if(view.isSelected())
+            if (view.isSelected())
                 return true;
         }
         return false;

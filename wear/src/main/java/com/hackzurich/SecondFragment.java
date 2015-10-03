ec70fedc153eaedcaba075c1b31c947098a681a1
ordon <ordon1313@gmail.com>
2015-10-03 14:02:57 +0200

    not a list but a list

--- wear/src/main/java/com/hackzurich/SecondFragment.java
+++ wear/src/main/java/com/hackzurich/SecondFragment.java
@@ -2,10 +2,10 @@ package com.hackzurich;
 
 import android.os.Bundle;
 import android.support.wearable.view.CardFragment;
-import android.support.wearable.view.WearableListView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hackzurich.model.Question;
 
@@ -13,6 +13,8 @@ public final class SecondFragment extends CardFragment {
 
     public static final String QUESTION = "question";
     private Question question;
+    private final int[] answerIds = {R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4, R.id.answer5, R.id.answer6, R.id.answer7};
+    private final int[] answerTextIds = {R.id.answerText1, R.id.answerText2, R.id.answerText3, R.id.answerText4, R.id.answerText5, R.id.answerText6, R.id.answerText7};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,8 +30,18 @@ public final class SecondFragment extends CardFragment {
     @Override
     public void onResume() {
         super.onResume();
-        WearableListView listView = (WearableListView) getView().findViewById(R.id.answer_list);
-        listView.setAdapter(new AnswersListAdapter(getActivity(), question));
+        for (int i = 0; i < question.getAnswers().size(); i++) {
+            View answerView = getView().findViewById(answerIds[i]);
+            answerView.setVisibility(View.VISIBLE);
+            answerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    v.setSelected(!v.isSelected());
+                }
+            });
+            TextView textView = (TextView) getView().findViewById(answerTextIds[i]);
+            textView.setText(question.getAnswers().get(i).getText());
+        }
     }
 
     public static SecondFragment instance(Question question) {

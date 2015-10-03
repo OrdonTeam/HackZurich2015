7e98b4a20235a5a3dd515b85edf02000f72e1f10
Szop-kradziej <karolina.drabarz@gmail.com>
2015-10-03 02:50:03 +0200

    Add QuestionActivity footer buttons listeners

--- mobile/src/main/java/com/hackzurich/QuestionActivity.java
+++ mobile/src/main/java/com/hackzurich/QuestionActivity.java
@@ -2,8 +2,8 @@ package com.hackzurich;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.hackzurich.adapter.AnswerGroupAdapter;
@@ -18,7 +18,7 @@ public class QuestionActivity extends Activity {
 
         setContentView(R.layout.question_activity);
         addAnswerListAdapter();
-
+        addFooterButtonsListeners();
         addExampleAnswers();
     }
 
@@ -26,6 +26,39 @@ public class QuestionActivity extends Activity {
         answerGroupAdapter = new AnswerGroupAdapter();
         ListView answerListView = (ListView) findViewById(R.id.answerListView);
         answerListView.setAdapter(answerGroupAdapter);
+    }
+
+    private void addFooterButtonsListeners() {
+        addDoNotKnowButtonListener();
+        addConfirmAnswerButtonListener();
+    }
+
+    private void addDoNotKnowButtonListener() {
+        Button doNotKnowButton = (Button) findViewById(R.id.doNotKnowButton);
+        doNotKnowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doNotKnowButtonClicked();
+            }
+        });
+    }
+
+    private void doNotKnowButtonClicked() {
+
+    }
+
+    private void addConfirmAnswerButtonListener() {
+        Button confirmAnswerButton = (Button) findViewById(R.id.confirmAnswerButton);
+        confirmAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmAnswerButtonClicked();
+            }
+        });
+    }
+
+    private void confirmAnswerButtonClicked() {
+
     }
 
     private void addExampleAnswers() {

3ea3294a26bcaade57f09bf9434fd0769a0ccfa2
ghostbuster91 <kghost0@gmail.com>
2015-10-04 02:36:29 +0200

    integration with dropbox api

--- mobile/src/main/java/com/hackzurich/HomeActivity.java
+++ mobile/src/main/java/com/hackzurich/HomeActivity.java
@@ -6,20 +6,32 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
 import com.hackzurich.connection.Listener;
 import com.hackzurich.connection.Sender;
 import com.hackzurich.database.TestIdRepository;
 import com.hackzurich.model.stub.TestFactory;
+import com.hackzurich.service.DropBoxService;
 import com.hackzurich.service.RepositoryService;
 import com.hackzurich.stub.STUB_PopulateDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Map;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class HomeActivity extends Activity {
+
+    private DropboxAPI<AndroidAuthSession> mDBApi;
+    private ArrayAdapter<String> arrayAdapter;
+    private DropBoxService dropBoxService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +40,8 @@ public class HomeActivity extends Activity {
         ButterKnife.bind(this);
 
         STUB_PopulateDatabase.populate(this);
-
+        dropBoxService = new DropBoxService();
+        dropBoxService.init();
         new Listener(this).start();
     }
 
@@ -43,6 +56,7 @@ public class HomeActivity extends Activity {
     @OnClick(R.id.download)
     public void onDownload() {
         Toast.makeText(this, "On download", Toast.LENGTH_LONG).show();
+        doDrop();
     }
 
     @OnClick(R.id.learn)
@@ -54,5 +68,40 @@ public class HomeActivity extends Activity {
     @OnClick(R.id.trigger_wear)
     public void onTriggerWear() {
         new Sender(this, TestFactory.newQuestion()).send();
+    }
+
+
+    void doDrop() {
+
+        dropBoxService.getAllFiles()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(new Action1<Map<String, DropboxAPI.Entry>>() {
+                    @Override
+                    public void call(Map<String, DropboxAPI.Entry> strings) {
+                        Log.e("kasper", strings.toString());
+                        dropBoxService.getSingleFile(HomeActivity.this, strings.get("/CapitalsOfEurope.json"))
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribeOn(Schedulers.newThread())
+                                .subscribe(new Action1<String>() {
+                                    @Override
+                                    public void call(String s) {
+                                        Log.e("kasper", "asdasd" + s);
+
+                                    }
+                                }, new Action1<Throwable>() {
+                                    @Override
+                                    public void call(Throwable throwable) {
+                                        Log.e("kasper", "error na drugim" + throwable.getMessage());
+                                    }
+                                });
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.e("kasper", throwable.getMessage());
+                    }
+                });
+
     }
 }
---
+++ mobile/src/main/java/com/hackzurich/service/DropBoxService.java
@@ -0,0 +1,132 @@
+package com.hackzurich.service;
+
+import android.content.Context;
+
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.exception.DropboxException;
+import com.dropbox.client2.session.AppKeyPair;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import rx.Observable;
+import rx.Subscriber;
+
+public class DropBoxService {
+
+    static private final String APP_KEY = "pzinws405fx9h6f";
+    static private final String APP_SECRET = "jlhtc6v52g7o8e5";
+
+    private DropboxAPI<AndroidAuthSession> mDBApi;
+
+    public void init() {
+        AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+        AndroidAuthSession session = new AndroidAuthSession(appKeys);
+        mDBApi = new DropboxAPI<>(session);
+        mDBApi.getSession().setOAuth2AccessToken("Wqmw-JGwSIAAAAAAAAAABoUgO-KGvzixClt9sc-J4NL_K_VUzlgZTCpDcLtGbGcM");
+    }
+
+    public Observable<String> getSingleFile(final Context context, final DropboxAPI.Entry entry) {
+        return Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                try {
+                    String content = getFileContent(entry, context);
+                    subscriber.onNext(content);
+                    subscriber.onCompleted();
+                } catch (Exception e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
+    public Observable<Map<String, DropboxAPI.Entry>> getAllFiles() {
+        return Observable.create(new Observable.OnSubscribe<Map<String, DropboxAPI.Entry>>() {
+            @Override
+            public void call(Subscriber<? super Map<String, DropboxAPI.Entry>> subscriber) {
+                try {
+                    Map<String, DropboxAPI.Entry> filesFromDropBox = getFilesFromDropBox();
+                    subscriber.onNext(filesFromDropBox);
+                    subscriber.onCompleted();
+                } catch (DropboxException e) {
+                    subscriber.onError(e);
+                }
+            }
+        });
+    }
+
+    private Map<String, DropboxAPI.Entry> getFilesFromDropBox() throws DropboxException {
+        DropboxAPI.Entry dirent = mDBApi.metadata("/", 1000, null, true, null);
+        Map<String, DropboxAPI.Entry> map = new HashMap<>();
+        for (DropboxAPI.Entry ent : dirent.contents) {
+            map.put(ent.path, ent);
+        }
+        return map;
+    }
+
+    public File createTmpFile(Context context, String name) throws IOException {
+        return File.createTempFile(name, null, context.getCacheDir());
+    }
+
+    private String getFileContent(final DropboxAPI.Entry fileSelected, Context context) throws Exception {
+        File localFile = createTmpFile(context, "tmpName");
+        BufferedInputStream bufferedInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+        DropboxAPI.DropboxInputStream dropboxInputStream;
+        try {
+            dropboxInputStream = mDBApi.getFileStream(fileSelected.path, fileSelected.rev);
+            bufferedInputStream = new BufferedInputStream(dropboxInputStream);
+            bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(localFile));
+
+            byte[] buffer = new byte[4096];
+            int read;
+            while (true) {
+                read = bufferedInputStream.read(buffer);
+                if (read <= 0) {
+                    break;
+                }
+                bufferedOutputStream.write(buffer, 0, read);
+            }
+        } finally {
+            if (bufferedOutputStream != null) {
+                bufferedOutputStream.close();
+                if (bufferedInputStream != null) {
+                    bufferedInputStream.close();
+                }
+            }
+        }
+        return extractContentFromLocalCopy(localFile.getAbsolutePath());
+    }
+
+    private static String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
+    }
+
+    private static String extractContentFromLocalCopy(String filePath) throws Exception {
+        File fl = new File(filePath);
+        FileInputStream fin = new FileInputStream(fl);
+        String ret = convertStreamToString(fin);
+        fin.close();
+        return ret;
+    }
+
+
+}
